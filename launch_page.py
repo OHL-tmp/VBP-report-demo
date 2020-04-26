219bee3dd87bab33b6149bd756f8028605ca746f
yanchen <rtaromax@hotmail.com>
2020-04-26 14:48:00 +0800

    add
    add launch page

--- launch_page.py
+++ launch_page.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -7,36 +9,55 @@ import dash_table
 import pandas as pd
 import numpy as np
 
-app = dash.Dash(__name__, url_base_pathname='/vbc-demo/launch/', external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, url_base_pathname='/vbc-demo/launch/')
 
 server = app.server
 
 def launch_layout():
     return dbc.Col([
+
                     html.Div(
                         [
-                            html.H2("ValueGen Solution"),
-                            html.Img(src=app.get_asset_url("logo-demo.png"), style={"height":"4rem", "padding-top":"1px"})
-                        ]
+                            
+                            html.Video(src=app.get_asset_url("launch_mesh.mov"), autoPlay=True, loop=True, style={"height":"45rem","border-bottom":"none", "text-align":"center"})
+                        ],
+                        style={"text-align":"center"}
                     ),
-                    dbc.Card([
-                        dbc.CardBody([
-                            dbc.CardLink("Contract Optimizer", href = "/vbc-demo/contract-optimizer/"),
-                        ]),
-                        dbc.CardBody([
-                            dbc.CardLink("Contract Manager", href = "/vbc-demo/contract-manager/"),  
-                        ]),
-                        dbc.CardBody([
-                            dbc.CardLink("Contract Validator", href = "/vbc-demo/contract-alidator/"),  
-                        ]),
-                        dbc.CardBody([
-                            dbc.CardLink("Tele Case Manager", href = "/vbc-demo/tele-case-manager/"),  
-                        ]),
-                    ]),
+                    html.Div(
+                        [
+                            html.P("Powered by One Health Link")
+                        ],
+                        style={"text-align":"center", "font-size":"0.6rem"}
+                    ),
+                    html.Div(
+                        [
+                            html.H1("ValueGen Solution",style={"background-color":"transparent","font-size":"5rem"}),
+                            dbc.Card([
+                                dbc.CardBody(
+                                    [
+                                        dbc.Row(
+                                            [
+                                                dbc.Col(dbc.Button("Contract Optimizer", color="light", className="mr-1", href = "/vbc-demo/contract-optimizer/", style={"font-family":"NotoSans-Regular", "font-size":"1rem", "padding":"1rem","border-radius":"1rem","border":"1ps solid #386010","box-shadow":"0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 20px 0 rgba(0, 0, 0, 0.05)"}), style={"border-radius":"1rem","width":"5rem"}),
+                                                dbc.Col(dbc.Button("Contract Manager", color="light", className="mr-1", href = "/vbc-demo/contract-manager/", style={"font-family":"NotoSans-Regular", "font-size":"1rem", "padding":"1rem", "padding":"1rem", "border-radius":"1rem","border":"1ps solid #386010","box-shadow":"0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 20px 0 rgba(0, 0, 0, 0.05)"}), style={"border-radius":"1rem","width":"5rem"}),
+                                                dbc.Col(dbc.Button("Contract Validator", color="light", className="mr-1", href = "/vbc-demo/contract-alidator/", style={"font-family":"NotoSans-Regular", "font-size":"1rem", "padding":"1rem", "border-radius":"1rem","border":"1ps solid #386010","box-shadow":"0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 20px 0 rgba(0, 0, 0, 0.05)"}), style={"border-radius":"1rem","width":"5rem"}),
+                                                dbc.Col(dbc.Button("Tele Case Manager", color="light", className="mr-1", href = "/vbc-demo/tele-case-manager/", style={"font-family":"NotoSans-Regular", "font-size":"1rem", "padding":"1rem", "border-radius":"1rem","border":"1ps solid #386010","box-shadow":"0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 20px 0 rgba(0, 0, 0, 0.05)"}), style={"border-radius":"1rem","width":"5rem"}),
+                                            ],
+                                            style={"background-color":"none", "font-family":"NotoSans-Regular", "font-size":"1rem", "border":"none","padding-top":"1rem","padding-bottom":"1rem","padding-left":"20rem","padding-right":"20rem"}
+                                        )
+                                    ]
+                                )
+
+                            ],
+                            style={"background-color":"transparent", "border":"none"}
+                            ),
+                        ],
+                        style={"margin-top":"-30rem","background-color":"transparent","text-align":"center"}
+                    )
+                    
     ])
         
 app.layout = launch_layout()        
 
 if __name__ == "__main__":
-    app.run_server(host="127.0.0.1",debug=False, port = 8052)
+    app.run_server(host="127.0.0.1", port = 8052)
                         
